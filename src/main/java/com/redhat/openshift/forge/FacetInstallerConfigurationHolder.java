c025e4744130b5860b90dff6a13bd02891efc8db
Rafael Benevides <rafabene@gmail.com>
2012-10-18 12:57:08 -0300

    FORGEPLUGINS-88, FORGEPLUGINS-89, FORGEPLUGINS-90, FORGEPLUGINS-91, FORGEPLUGINS-92, FORGEPLUGINS-93 and FORGEPLUGINS-94

--- src/main/java/com/redhat/openshift/forge/FacetInstallerConfigurationHolder.java
+++ src/main/java/com/redhat/openshift/forge/FacetInstallerConfigurationHolder.java
@@ -4,26 +4,49 @@ import org.jboss.forge.shell.project.ProjectScoped;
 
 @ProjectScoped
 public class FacetInstallerConfigurationHolder {
-   
-   private String rhLogin;
-   private String name;
-   
-   public String getRhLogin() {
-      return rhLogin;
-   }
-   public void setRhLogin(String rhLogin) {
-      this.rhLogin = rhLogin;
-   }
-   public String getName() {
-      return name;
-   }
-   public void setName(String name) {
-      this.name = name;
-   }
-   
-   public void clear() {
-      this.name = null;
-      this.rhLogin = null;
-   }
+
+    private String rhLogin;
+    private String name;
+    private String gitRemoteRepo;
+    private boolean scaling;
+
+    public String getRhLogin() {
+        return rhLogin;
+    }
+
+    public void setRhLogin(String rhLogin) {
+        this.rhLogin = rhLogin;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getGitRemoteRepo() {
+        return gitRemoteRepo;
+    }
+
+    public void setGitRemoteRepo(String gitRemoteRepo) {
+        this.gitRemoteRepo = gitRemoteRepo;
+    }
+
+    public boolean isScaling() {
+        return scaling;
+    }
+
+    public void setScaling(boolean scaling) {
+        this.scaling = scaling;
+    }
+
+    public void clear() {
+        this.name = null;
+        this.rhLogin = null;
+        this.gitRemoteRepo = null;
+        this.scaling = false;
+    }
 
 }
---
+++ src/main/java/com/redhat/openshift/forge/jsch/JschToForgeLogger.java
@@ -0,0 +1,74 @@
+/*
+ * JBoss, Home of Professional Open Source.
+ * Copyright 2012, Red Hat, Inc., and individual contributors
+ * as indicated by the @author tags. See the copyright.txt file in the
+ * distribution for a full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+
+package com.redhat.openshift.forge.jsch;
+
+import javax.inject.Inject;
+
+import org.jboss.forge.shell.Shell;
+import org.jboss.forge.shell.ShellMessages;
+
+/**
+ * @author <a href="mailto:benevides@redhat.com">Rafael Benevides</a>
+ * 
+ */
+public class JschToForgeLogger implements com.jcraft.jsch.Logger {
+
+    @Inject
+    private Shell shell;
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see com.jcraft.jsch.Logger#isEnabled(int)
+     */
+    @Override
+    public boolean isEnabled(int level) {
+        if (level == DEBUG || level == INFO) {
+            return shell.isVerbose();
+        }
+        return true;
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see com.jcraft.jsch.Logger#log(int, java.lang.String)
+     */
+    @Override
+    public void log(int level, String message) {
+        switch (level) {
+            case DEBUG:
+            case INFO:
+                ShellMessages.info(shell, message);
+                break;
+            case WARN:
+                ShellMessages.warn(shell, message);
+                break;
+            case ERROR:
+            case FATAL:
+                ShellMessages.error(shell, message);
+                break;
+        }
+
+    }
+}
